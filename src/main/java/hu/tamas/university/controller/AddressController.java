6297fdc42e36a58d33fd5319a07f674e86299de8
Tamas Komuves <komuves.tomi@gmail.com>
2019-03-30 16:06:34 +0100

    Add update address endpoint

--- src/main/java/hu/tamas/university/controller/AddressController.java
+++ src/main/java/hu/tamas/university/controller/AddressController.java
@@ -18,19 +18,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class AddressController {
 
 	private final AddressRepository addressRepository;
+	private final HttpHeaders headers = new HttpHeaders();
 
 	@Autowired
 	public AddressController(AddressRepository addressRepository) {
 		this.addressRepository = addressRepository;
+		headers.add("Access-Control-Allow-Origin", "*");
 	}
 
 	@GetMapping("/{id}")
 	public @ResponseBody
 	ResponseEntity<AddressDto> getAddressById(@PathVariable int id) {
 		Address tmpAddress = addressRepository.findAddressById(id);
-
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Access-Control-Allow-Origin", "*");
 
 		return new ResponseEntity<>(AddressDto.fromEntity(tmpAddress), headers, HttpStatus.OK);
 	}
@@ -47,11 +46,29 @@ public class AddressController {
 
 		address = addressRepository.save(address);
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Access-Control-Allow-Origin", "*");
-
 		return new ResponseEntity<>("{\"result\":\"success\", " +
 				"\"addressId\":\"" + address.getId() + "\"}", headers,
 				HttpStatus.OK);
+	}
+
+	@GetMapping("/update/{addressId}/{country}/{city}/{street}/{streetNumber}")
+	public @ResponseBody
+	ResponseEntity<String> updateAddress(@PathVariable int addressId, @PathVariable String country,
+	                                   @PathVariable String city,
+	                                   @PathVariable String street, @PathVariable String streetNumber) {
+		Address address = addressRepository.findAddressById(addressId);
+
+		if (address == null) {
+			return new ResponseEntity<>("{\"result\":\"no such address\"}", headers, HttpStatus.OK);
+		}
+
+		address.setCountry(country);
+		address.setCity(city);
+		address.setStreet(street);
+		address.setStreetNumber(streetNumber);
+
+		addressRepository.save(address);
+
+		return new ResponseEntity<>("{\"result\":\"success\"}", headers, HttpStatus.OK);
 	}
 }
