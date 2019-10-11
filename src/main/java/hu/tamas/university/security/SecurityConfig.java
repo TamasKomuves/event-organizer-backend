81207233f705391a447d8c6df6bf98c2059de157
Tamas Komuves <komuves.tomi@gmail.com>
2019-10-11 21:39:08 +0200

    Reformat SecurityConfig

--- src/main/java/hu/tamas/university/security/SecurityConfig.java
+++ src/main/java/hu/tamas/university/security/SecurityConfig.java
@@ -31,80 +31,80 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @FieldDefaults(level = PRIVATE, makeFinal = true)
 class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  private static final RequestMatcher PUBLIC_URLS = new OrRequestMatcher(
-          new AntPathRequestMatcher("/public/**"),
-          new AntPathRequestMatcher("/error/**")
-  );
-  private static final RequestMatcher PROTECTED_URLS = new NegatedRequestMatcher(PUBLIC_URLS);
+	private static final RequestMatcher PUBLIC_URLS = new OrRequestMatcher(
+			new AntPathRequestMatcher("/public/**"),
+			new AntPathRequestMatcher("/error/**")
+	);
+	private static final RequestMatcher PROTECTED_URLS = new NegatedRequestMatcher(PUBLIC_URLS);
 
-  private TokenAuthenticationProvider tokenAuthenticationProvider;
+	private TokenAuthenticationProvider tokenAuthenticationProvider;
 
-  SecurityConfig(final TokenAuthenticationProvider tokenAuthenticationProvider) {
-    super();
-    this.tokenAuthenticationProvider = requireNonNull(tokenAuthenticationProvider);
-  }
+	SecurityConfig(final TokenAuthenticationProvider tokenAuthenticationProvider) {
+		super();
+		this.tokenAuthenticationProvider = requireNonNull(tokenAuthenticationProvider);
+	}
 
-  @Override
-  protected void configure(final AuthenticationManagerBuilder auth) {
-    auth.authenticationProvider(tokenAuthenticationProvider);
-  }
+	@Override
+	protected void configure(final AuthenticationManagerBuilder auth) {
+		auth.authenticationProvider(tokenAuthenticationProvider);
+	}
 
-  @Override
-  public void configure(final WebSecurity web) {
-    web.ignoring().requestMatchers(PUBLIC_URLS);
-  }
+	@Override
+	public void configure(final WebSecurity web) {
+		web.ignoring().requestMatchers(PUBLIC_URLS);
+	}
 
-  @Override
-  protected void configure(final HttpSecurity http) throws Exception {
-    http
-				    .addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class)
-            .sessionManagement()
-            .sessionCreationPolicy(STATELESS)
-            .and()
-            .exceptionHandling()
-            // this entry point handles when you request a protected page and you are not yet
-            // authenticated
-            .defaultAuthenticationEntryPointFor(forbiddenEntryPoint(), PROTECTED_URLS)
-            .and()
-            .authenticationProvider(tokenAuthenticationProvider)
-            .addFilterBefore(restAuthenticationFilter(), AnonymousAuthenticationFilter.class)
-            .authorizeRequests()
-            .requestMatchers(PROTECTED_URLS)
-            .authenticated()
-            .and()
-            .csrf().disable()
-            .formLogin().disable()
-            .httpBasic().disable()
-            .logout().disable();
-  }
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+		http
+				.addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class)
+				.sessionManagement()
+				.sessionCreationPolicy(STATELESS)
+				.and()
+				.exceptionHandling()
+				// this entry point handles when you request a protected page and you are not yet
+				// authenticated
+				.defaultAuthenticationEntryPointFor(forbiddenEntryPoint(), PROTECTED_URLS)
+				.and()
+				.authenticationProvider(tokenAuthenticationProvider)
+				.addFilterBefore(restAuthenticationFilter(), AnonymousAuthenticationFilter.class)
+				.authorizeRequests()
+				.requestMatchers(PROTECTED_URLS)
+				.authenticated()
+				.and()
+				.csrf().disable()
+				.formLogin().disable()
+				.httpBasic().disable()
+				.logout().disable();
+	}
 
-  @Bean
-  TokenAuthenticationFilter restAuthenticationFilter() throws Exception {
-    final TokenAuthenticationFilter filter = new TokenAuthenticationFilter(PROTECTED_URLS);
-    filter.setAuthenticationManager(authenticationManager());
-    filter.setAuthenticationSuccessHandler(successHandler());
-    return filter;
-  }
+	@Bean
+	TokenAuthenticationFilter restAuthenticationFilter() throws Exception {
+		final TokenAuthenticationFilter filter = new TokenAuthenticationFilter(PROTECTED_URLS);
+		filter.setAuthenticationManager(authenticationManager());
+		filter.setAuthenticationSuccessHandler(successHandler());
+		return filter;
+	}
 
-  @Bean
-  SimpleUrlAuthenticationSuccessHandler successHandler() {
-    final SimpleUrlAuthenticationSuccessHandler successHandler = new SimpleUrlAuthenticationSuccessHandler();
-    successHandler.setRedirectStrategy(new NoRedirectStrategy());
-    return successHandler;
-  }
+	@Bean
+	SimpleUrlAuthenticationSuccessHandler successHandler() {
+		final SimpleUrlAuthenticationSuccessHandler successHandler = new SimpleUrlAuthenticationSuccessHandler();
+		successHandler.setRedirectStrategy(new NoRedirectStrategy());
+		return successHandler;
+	}
 
-  /**
-   * Disable Spring boot automatic filter registration.
-   */
-  @Bean
-  FilterRegistrationBean disableAutoRegistration(final TokenAuthenticationFilter filter) {
-    final FilterRegistrationBean registration = new FilterRegistrationBean(filter);
-    registration.setEnabled(false);
-    return registration;
-  }
+	/**
+	 * Disable Spring boot automatic filter registration.
+	 */
+	@Bean
+	FilterRegistrationBean disableAutoRegistration(final TokenAuthenticationFilter filter) {
+		final FilterRegistrationBean registration = new FilterRegistrationBean(filter);
+		registration.setEnabled(false);
+		return registration;
+	}
 
-  @Bean
-  AuthenticationEntryPoint forbiddenEntryPoint() {
-    return new HttpStatusEntryPoint(FORBIDDEN);
-  }
+	@Bean
+	AuthenticationEntryPoint forbiddenEntryPoint() {
+		return new HttpStatusEntryPoint(FORBIDDEN);
+	}
 }
