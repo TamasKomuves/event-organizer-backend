9b6791cd9cb263e550fb826e45a58152d801ff0a
Tamas Komuves <komuves.tomi@gmail.com>
2019-10-26 16:49:33 +0200

    Fix CommentController after upgrade

--- src/main/java/hu/tamas/university/controller/CommentController.java
+++ src/main/java/hu/tamas/university/controller/CommentController.java
@@ -1,12 +1,18 @@
 package hu.tamas.university.controller;
 
+import com.google.common.collect.Lists;
 import hu.tamas.university.dto.CommentDto;
 import hu.tamas.university.dto.UserDto;
 import hu.tamas.university.entity.Comment;
+import hu.tamas.university.entity.LikesComment;
+import hu.tamas.university.entity.Post;
+import hu.tamas.university.entity.User;
 import hu.tamas.university.repository.CommentRepository;
+import hu.tamas.university.repository.LikesCommentRepository;
 import hu.tamas.university.repository.PostRepository;
 import hu.tamas.university.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Controller
@@ -24,18 +31,21 @@ public class CommentController {
 	private final CommentRepository commentRepository;
 	private final PostRepository postRepository;
 	private final UserRepository userRepository;
+	private final LikesCommentRepository likesCommentRepository;
 
 	@Autowired
-	public CommentController(CommentRepository commentRepository, PostRepository postRepository, UserRepository userRepository) {
+	public CommentController(CommentRepository commentRepository, PostRepository postRepository,
+			UserRepository userRepository, LikesCommentRepository likesCommentRepository) {
 		this.commentRepository = commentRepository;
 		this.postRepository = postRepository;
 		this.userRepository = userRepository;
+		this.likesCommentRepository = likesCommentRepository;
 	}
 
 	@GetMapping("/{id}")
 	@ResponseBody
 	public CommentDto getCommentById(@PathVariable int id) {
-		Comment comment = commentRepository.findCommentById(id);
+		final Comment comment = commentRepository.findCommentById(id);
 
 		return CommentDto.fromEntity(comment);
 	}
@@ -43,21 +53,27 @@ public class CommentController {
 	@GetMapping("/{id}/likers")
 	@ResponseBody
 	public List<UserDto> getLikers(@PathVariable int id) {
-		Comment comment = commentRepository.findCommentById(id);
+		final List<LikesComment> likesComments = likesCommentRepository.findByCommentId(id)
+				.orElse(Lists.newArrayList());
 
-		return comment.getLikers().stream().map(UserDto::fromEntity).collect(Collectors.toList());
+		return likesComments.stream().map(likesComment -> UserDto.fromEntity(likesComment.getUser()))
+				.collect(Collectors.toList());
 	}
 
 	@GetMapping("create/{postId}/{commenter_email}/{text}")
 	@ResponseBody
-	public String saveComment(@PathVariable int postId, @PathVariable String commenter_email, @PathVariable String text) {
-		Comment comment = new Comment();
-		comment.setPost(postRepository.findPostById(postId));
-		comment.setCommenter(userRepository.findByEmail(commenter_email).get());
+	public String saveComment(@PathVariable int postId, @PathVariable String commenter_email,
+			@PathVariable String text) {
+		final Post post = postRepository.findPostById(postId);
+		final User user = userRepository.findByEmail(commenter_email).orElse(null);
+
+		final Comment comment = new Comment();
 		comment.setText(text);
 		comment.setCommentDate(new Timestamp(System.currentTimeMillis()));
+		post.addComment(comment);
+		user.addComment(comment);
 
-		commentRepository.save(comment);
+		commentRepository.saveAndFlush(comment);
 
 		return "{\"result\":\"success\"}";
 	}
@@ -65,11 +81,20 @@ public class CommentController {
 	@GetMapping("{id}/likers/{email}")
 	@ResponseBody
 	public String isLikedAlready(@PathVariable int id, @PathVariable String email) {
-		Comment comment = commentRepository.findCommentById(id);
-		int number = comment.getLikers().stream().
-				filter(user -> user.getEmail().equals(email)).collect(Collectors.toList()).size();
-		String result = number > 0 ? "true" : "false";
+		final Optional<LikesComment> likesComment = likesCommentRepository.findByCommentIdAndUserEmail(id, email);
 
-		return "{\"result\":\"" + result + "\"}";
+		return "{\"result\":\"" + likesComment.isPresent() + "\"}";
+	}
+
+	@GetMapping("{id}/add-liker")
+	@ResponseBody
+	public String addLiker(@PathVariable int id, @AuthenticationPrincipal User user) {
+		final Comment comment = commentRepository.findCommentById(id);
+		final User liker = userRepository.findByEmail(user.getEmail()).get();
+
+		comment.addLiker(liker);
+		commentRepository.flush();
+
+		return "{\"result\":\"success\"}";
 	}
 }
--- src/main/java/hu/tamas/university/repository/LikesCommentRepository.java
+++ src/main/java/hu/tamas/university/repository/LikesCommentRepository.java
@@ -3,9 +3,14 @@ package hu.tamas.university.repository;
 import hu.tamas.university.entity.LikesComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LikesCommentRepository extends JpaRepository<LikesComment, Integer> {
 
 	LikesComment findLikesCommentById(int id);
 
+	Optional<List<LikesComment>> findByCommentId(int commentId);
 
+	Optional<LikesComment> findByCommentIdAndUserEmail(int commentId, String userEmail);
 }
