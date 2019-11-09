8513230136aacc1b9b977172230a9dea5636efed
Tamas Komuves <komuves.tomi@gmail.com>
2019-11-09 19:47:55 +0100

    Add @NotNull annotation to PasswordChangeDto's fields

--- src/main/java/hu/tamas/university/dto/PasswordChangeDto.java
+++ src/main/java/hu/tamas/university/dto/PasswordChangeDto.java
@@ -3,13 +3,18 @@ package hu.tamas.university.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class PasswordChangeDto {
 
+	@NotNull
 	private String oldPassword;
 
+	@NotNull
 	private String password;
 
+	@NotNull
 	private String passwordAgain;
 }
