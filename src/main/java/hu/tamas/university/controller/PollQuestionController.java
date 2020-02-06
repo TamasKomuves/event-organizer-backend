b4b74e3ffe96cd222546edd813a24bdc50867279
Tamas Komuves <komuves.tomi@gmail.com>
2020-02-07 00:52:29 +0100

    Add delete pollquestion endpoint

--- src/main/java/hu/tamas/university/controller/PollQuestionController.java
+++ src/main/java/hu/tamas/university/controller/PollQuestionController.java
@@ -2,8 +2,10 @@ package hu.tamas.university.controller;
 
 import hu.tamas.university.dto.PollQuestionDto;
 import hu.tamas.university.dto.creatordto.PollCreatorDto;
+import hu.tamas.university.entity.User;
 import hu.tamas.university.service.PollQuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,8 @@ import java.util.List;
 @Controller
 @RequestMapping("/poll-questions")
 public class PollQuestionController {
+
+	private static final String RESULT_SUCCESS = "{\"result\":\"success\"}";
 
 	private final PollQuestionService pollQuestionService;
 
@@ -37,6 +41,13 @@ public class PollQuestionController {
 	@ResponseBody
 	public String createPoll(@RequestBody @Valid PollCreatorDto pollCreatorDto) {
 		pollQuestionService.createPoll(pollCreatorDto);
-		return "{\"result\":\"success\"}";
+		return RESULT_SUCCESS;
+	}
+
+	@DeleteMapping("{id}/delete")
+	@ResponseBody
+	public String deletePollQuestion(@PathVariable int id, @AuthenticationPrincipal User user) {
+		pollQuestionService.deletePollQuestion(id, user.getEmail());
+		return RESULT_SUCCESS;
 	}
 }
--- src/main/java/hu/tamas/university/service/PollQuestionService.java
+++ src/main/java/hu/tamas/university/service/PollQuestionService.java
@@ -6,14 +6,21 @@ import hu.tamas.university.dto.creatordto.PollCreatorDto;
 import hu.tamas.university.entity.Event;
 import hu.tamas.university.entity.PollAnswer;
 import hu.tamas.university.entity.PollQuestion;
+import hu.tamas.university.entity.User;
+import hu.tamas.university.repository.AnswersToPollRepository;
 import hu.tamas.university.repository.EventRepository;
 import hu.tamas.university.repository.PollAnswerRepository;
 import hu.tamas.university.repository.PollQuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import java.sql.Timestamp;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,14 +29,18 @@ public class PollQuestionService {
 	private final PollQuestionRepository pollQuestionRepository;
 	private final PollAnswerRepository pollAnswerRepository;
 	private final EventRepository eventRepository;
+	private final AnswersToPollRepository answersToPollRepository;
+	private final EntityManager entityManager;
 
 	@Autowired
 	public PollQuestionService(PollQuestionRepository pollQuestionRepository,
-			PollAnswerRepository pollAnswerRepository,
-			EventRepository eventRepository) {
+			PollAnswerRepository pollAnswerRepository, EventRepository eventRepository,
+			AnswersToPollRepository answersToPollRepository, EntityManagerFactory entityManagerFactory) {
 		this.pollQuestionRepository = pollQuestionRepository;
 		this.pollAnswerRepository = pollAnswerRepository;
 		this.eventRepository = eventRepository;
+		this.answersToPollRepository = answersToPollRepository;
+		this.entityManager = entityManagerFactory.createEntityManager();
 	}
 
 	public PollQuestionDto getPollQuetionById(final int id) {
@@ -60,5 +71,32 @@ public class PollQuestionService {
 		final Event event = eventRepository.findEventById(pollCreatorDto.getEventId());
 		event.addPollQuestion(pollQuestion);
 		eventRepository.flush();
+	}
+
+	public void deletePollQuestion(final int id, final String userEmail) {
+		if (!hasPermissionToDeletePollQuestion(id, userEmail)) {
+			throw new RuntimeException("no permission");
+		}
+
+		final Optional<List<Integer>> pollAnswerIdsOptional = pollAnswerRepository
+				.findIdByPollQuestionIds(Collections.singletonList(id));
+
+		final EntityTransaction transaction = entityManager.getTransaction();
+		try {
+			transaction.begin();
+			pollAnswerIdsOptional.ifPresent(answersToPollRepository::deleteByPollAnswerIdIn);
+			pollAnswerRepository.deleteByPollQuestionIdIn(Collections.singletonList(id));
+			pollQuestionRepository.deleteById(id);
+			transaction.commit();
+		} catch (Exception e) {
+			transaction.rollback();
+			throw e;
+		}
+	}
+
+	private boolean hasPermissionToDeletePollQuestion(final int id, final String userEmail) {
+		final PollQuestion pollQuestion = pollQuestionRepository.findPollQuestionById(id);
+		final User organizer = pollQuestion.getEvent().getOrganizer();
+		return organizer != null && userEmail.equals(organizer.getEmail());
 	}
 }
